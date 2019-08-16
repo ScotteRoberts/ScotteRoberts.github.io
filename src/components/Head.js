c55ba5c40f6e4f1b60f0bd3ff81a4b7a7caf2ee8
Scott Roberts <s.e.roberts0@gmail.com>
2019-08-16 08:36:10 -0700

    Change favicon location

--- src/components/Head.js
+++ src/components/Head.js
@@ -8,7 +8,11 @@ const Head = ({ title }) => (
     <meta charset="utf-8" />
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico" />
+    <link
+      rel="shortcut icon"
+      type="image/x-icon"
+      href="../assets/img/favicon.ico"
+    />
     <meta
       name="Description"
       content="Creator: Scott Roberts, Role: Full-Stack Engineer Consultant, Company: Talent Path, Blog: Technical Ramblings"
